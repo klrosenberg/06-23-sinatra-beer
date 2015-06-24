1c139998f765afdfd8e91bccd5092b3b4213031c
Kerry Rosenberg <rosenberg.kerry@gmail.com>
2015-06-24 12:49:17 -0500

    Updated documentation.

--- models/beer_type.rb
+++ models/beer_type.rb
@@ -8,11 +8,19 @@ class BeerType
   attr_reader :id
   attr_accessor :name
 
+  # Instantaites a new instance of the BeerType Class.
+  # 
+  # options = {}- optional argument Hash
+  # 
+  # Returns new object of the BeerType Class.
   def initialize(options = {})
     @id = options['id']
     @name = options['name']
   end
-
+  
+  # Updates beer type in database.
+  #
+  # Returns Boolean.
   def save
     if
       DATABASE.execute("UPDATE beer_types SET name = '#{@name}' WHERE id = #{id};")
