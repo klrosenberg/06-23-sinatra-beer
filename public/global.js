e7f1ee79683a5742dc4dfb719617a5af5f08034a
Kerry Rosenberg <rosenberg.kerry@gmail.com>
2015-07-09 13:33:10 -0500

    Hamburger menu- icon completed, working on functionality

---
+++ public/global.js
@@ -0,0 +1,5 @@
+// Changes the visibility property of the element (toggle)
+function overlay() {
+  modal = document.getElementById("responsive_menu");
+  modal.style.visibility = (modal.style.visibility == "visible") ? "hidden": "visible";
+}
