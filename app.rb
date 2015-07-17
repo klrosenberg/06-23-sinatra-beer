bd0882c1a3b898a92dd4a5979106cce590e51207
Kerry Rosenberg <rosenberg.kerry@gmail.com>
2015-07-17 10:58:12 -0500

    Added production/development environments

--- app.rb
+++ app.rb
@@ -2,14 +2,29 @@ require 'rubygems'
 require 'bundler/setup'
 
 require "pry"
-require "active_record"
 require "sinatra"
 require "sinatra/reloader"
 require "bcrypt"
 require "active_support"
 require "active_support/inflector"
+require "active_record"
 
+configure :development do
+  ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'beerarchive.db')
+end
 
+configure :production do  
+  db = URI.parse(ENV['DATABASE_URL'])
+
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
 
 set :sessions, true
 
